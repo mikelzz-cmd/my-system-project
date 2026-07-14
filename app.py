0f6decab420a32ca14cc871f19509900f955f67e
Mikelzz-cmd <mikelgalang25@gmail.com>
2026-07-14 14:26:31 +0800

    Add DB_PORT support

--- app.py
+++ app.py
@@ -123,6 +123,7 @@ db_pool = mysql.connector.pooling.MySQLConnectionPool(
     pool_name="coffee_pool",
     pool_size=10,
     host=os.getenv("DB_HOST", "localhost"),
+    port=int(os.getenv("DB_PORT", 3306)),
     user=os.getenv("DB_USER", "root"),
     password=os.getenv("DB_PASSWORD", ""),
     database=os.getenv("DB_NAME", "coffee_shop")
